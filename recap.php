efabbd99570eaa902adc1f8dab0d2350507d3a16
angalland <angalland@laposte.net>
2023-05-11 09:13:03 +0200

    ajout d'une condition dans recap.php

--- recap.php
+++ recap.php
@@ -12,9 +12,12 @@
 </head>
 <body>
     <?php
-        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-            echo ""
-        }fefef
+      if(isset($_SESSION['products']) || empty($_SESSION['products'])){
+        echo "<p>Aucun produit en session....</p>";
+      }
+      else{
+        
+      }
      ?>
 </body>
 </html>
