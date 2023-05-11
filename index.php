ab69d6ea5ef8cad71c38d3bc31c2eca4fc617238
angalland <angalland@laposte.net>
2023-05-11 18:56:30 +0200

    ajout d'un message d'alert dans traitement.php

--- index.php
+++ index.php
@@ -45,6 +45,12 @@
                 </p>
             </form>
             <p class="bg-success w-25 rounded-pill text-center text-white">Il y a <?= count($_SESSION['products']) ?> produits ajoutés</p>
+            <?php 
+                if (isset($_SESSION['alert'])){
+                    echo $_SESSION['alert'];
+                    unset($_SESSION['alert']);
+                }
+            ?>
         </main> 
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
