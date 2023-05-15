4c49b0df96017db2582cda489bd8ebf5fedd3d3d
angalland <angalland@laposte.net>
2023-05-15 09:25:20 +0200

    ajout de commentaire

--- traitement_qtt.php
+++ traitement_qtt.php
@@ -1,21 +1,21 @@
 <?php
-session_start();
+session_start(); // fait appelle a la session
 
-if(isset($_POST['submit'])){
-    $indexInput = filter_input(INPUT_POST, "productIndex", FILTER_VALIDATE_INT);
-    $qttModifier = filter_input(INPUT_POST, "modifierQtt");
+if(isset($_POST['submit'])){ 
+    $indexInput = filter_input(INPUT_POST, "productIndex", FILTER_VALIDATE_INT); // créer une variable = a la valeur de l'index recuperer via type=hidden
+    $qttModifier = filter_input(INPUT_POST, "modifierQtt"); // créer une variable = a la valeur de 'modifierQtt' récuperer via type=hidden
 
-    foreach ($_SESSION['products'] as $index => $product) {
-        if ($index == $indexInput) {
-            if ($qttModifier == '+') {
-                $_SESSION['products'][$index]['qtt'] +=1;
-            } else {
-                if ($_SESSION['products'][$index]['qtt'] > 0){
-                $_SESSION['products'][$index]['qtt'] -=1;
+    foreach ($_SESSION['products'] as $index => $product) { // on fait une boucle sur $_SESSION['products'] en lui assignant un index
+        if ($index == $indexInput) {  // si l'index = l'index récuperer plus haut alors
+            if ($qttModifier == '+') {  // si la valeur de qttModifier récupérer plus haut = '+' alors
+                $_SESSION['products'][$index]['qtt'] +=1;  // on ajoute 1 a la valeur de qtt dans le tableau SESSION
+            } else { //sinon 
+                if ($_SESSION['products'][$index]['qtt'] > 0){  // si la valeur de qtt dans le tableau SESSION > 0 alors
+                $_SESSION['products'][$index]['qtt'] -=1; // on enleve 1 a la valeur de qtt dans le tableau SESSION
                 }
             }
         }
     }
 }
 
-header('Location:recap.php');
+header('Location:recap.php'); // renvoie sur la page recap.php
