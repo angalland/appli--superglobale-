ca8993449f3040e2ab5b5ef26df273fd4355d2e4
angalland <angalland@laposte.net>
2023-05-15 19:26:33 +0200

    ajout d'un input type=file et enctype dans form

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <ul class="nav justify-content-center nav-pills">
                     <li class="nav-item"><a class="nav-link active me-3" href="index.php">Ajouter un produit</a></li>
                     <li class="nav-item position-relative">
-                        <a class="nav-link active" href="recap.php">
+                        <a class="nav-link active" href="recap copy.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
                             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
                             </svg>  <!-- importe une icone bootstraps en forme de panier -->
@@ -37,7 +37,7 @@
                 <ul>
             </nav>
             <h1 class="row col-12">Ajouter un produit</h1>
-            <form action="traitement.php?action=addProduct" method="post" class="row col-12 justify-content-center"> <!-- formulaire pour ajouter un produit, les données sont envoyé sur la page traitement.php avec la methode post -->
+            <form action="traitement.php?action=addProduct" method="post" enctype="multipart/form-data" class="row col-12 justify-content-center"> <!-- formulaire pour ajouter un produit, les données sont envoyé sur la page traitement.php avec la methode post -->
                 <p class="col-12">
                     <label>
                         Nom du produit : 
@@ -55,6 +55,10 @@
                         Quantité désirée : 
                         <input type="number" name="qtt" value="1" class="form-control">
                     </label>
+                </p>
+                <p class="col-12">
+                    <label for="file">Fichier</label>
+                    <input type="file" name="file"> 
                 </p>
                 <p>
                     <input type="submit" name="submit" value="Ajouter le produit" class="btn btn-primary" id="boutton">
