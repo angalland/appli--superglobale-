e69f1244de8404b60706fa0b14b87bbc50d7974c
angalland <angalland@laposte.net>
2023-05-15 14:34:14 +0200

    ajout de php pour enregistrer img

--- importerImage/indexTuto.php
+++ importerImage/indexTuto.php
@@ -1,3 +1,16 @@
+<?php
+
+    if (isset($_FILES["file"])){
+        $tmpName = $_FILES["file"]['tmp_name'];
+        $name = $_FILES["file"]["name"];
+        $type = $_FILES["file"]["type"];
+        $error = $_FILES["file"]["error"];
+        $size = $_FILES["file"]["size"];
+    
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +20,11 @@
     <title>Tuto importer une image</title>
 </head>
 <body>
-    <p>Hello world</p>
+    <form action="indexTuto.php" method="post" enctype="multipart/form-data">
+        <label for="file">Fichier</label>
+        <input type="file" name="file">
+        <button type="submit">Enregistrer</button>
+    </form>
+
 </body>
 </html>
