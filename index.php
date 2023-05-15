3538312bdce87902605e543f4cfcc1c45b152858
angalland <angalland@laposte.net>
2023-05-15 08:52:42 +0200

    ajout de commentaire

--- index.php
+++ index.php
@@ -5,23 +5,24 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- import le css bootsrapt -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"       integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">    
 
         <title>Ajout produit</title>
     </head>
     <body>
         <?php
-            session_start();
+            session_start(); // Fait appelle a la session
         ?>
         <main class="container my-5">
-            <nav>
+            <nav> <!-- menu -->
                 <ul class="nav justify-content-center nav-pills">
                     <li class="nav-item"><a class="nav-link" href="index.php">Ajouter un produit</a></li>
                     <li class="nav-item"><a class="nav-link active" href="recap.php">Récapitulatif</a></li>
                 <ul>
             </nav>
             <h1 class="row col-12">Ajouter un produit</h1>
-            <form action="traitement.php" method="post" class="row col-12 justify-content-center">
+            <form action="traitement.php" method="post" class="row col-12 justify-content-center"> <!-- formulaire pour ajouter un produit, les données sont envoyé sur la page traitement.php avec la methode post -->
                 <p class="col-12">
                     <label>
                         Nom du produit : 
@@ -46,22 +47,23 @@
             </form>
             <p class="bg-success w-25 rounded-pill text-center text-white">
                 <?php
-                 if(($_SESSION) != null) {
-                echo "Il y a ".count($_SESSION['products'])." produits ajoutés.";
+                 if(($_SESSION) != null) { // Si il y a un $_SESSION de créer alors
+                echo "Il y a ".count($_SESSION['products'])." produits ajoutés."; // renvoie le nombre total d'element dans $_SESSION['products']
                 } else {
-                    echo "Aucun produit ajouté.";
+                    echo "Aucun produit ajouté."; // sinon renvoie cette phrase
                 }; 
                 ?> 
             </p>
              <?php 
-                if (isset($_SESSION['alert'])){
-                    echo $_SESSION['alert'];
-                    unset($_SESSION['alert']);
+                if (isset($_SESSION['alert'])){ // si il y a un $_SESSION['alert'] alors
+                    echo $_SESSION['alert'];    // renvoie $_SESSION['alert']
+                    unset($_SESSION['alert']);  //Détruit $_SESSION['alert'] des qu'on recharge la page, c'est pour que le message d'alert ne reste pas permanent sur la page
                 }
             ?>
            </p>
         </main> 
 
+        <!-- Importe le js bootstrapt -->
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     </body>
--- traitement.php
+++ traitement.php
@@ -1,37 +1,40 @@
 <?php
-    session_start();
+    session_start(); // enregistre des données sur le serveur, ici on demarre une session. Cette fonction permet soit de demarrer une session soit de récupérer une séssion déja présente sur le serveur
 
     $nbSession = 0;
 
-    if(isset($_POST['submit'])){
+    if(isset($_POST['submit'])){ // Vérification que l'utilisateur a bien appuyer sur le bouton ajouter produit
+
+        // Filtre pour vérifié les données saisies par l'utilisateur
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
         $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
-        $_SESSION['alert'] = "<p class='alert alert-success w-25' role='alert'>Le produit".$product['name']." a bien été ajouté !</p>";
-        
 
-        if($name && $price && $qtt){
+        //Envoie un message lorsque l'utilisateur a ajouté un produit       
 
-            $product = [
+        if($name && $price && $qtt){ // Cette condition vérifie si les filtres ont bien fonctionné, que les variables renvoient tous sauf false, null ou 0.
+
+            $product = [ //Mets les information d'un produits dans un tableau assiocatif
                 "name"  => $name,
                 "price" => $price,
                 "qtt"   => $qtt,
-                // "total" => $price*$qtt
+                // "total" => $price*$qtt sera traité directement sur la page recap.php
             ];
 
-            if (isset($_GET['+'])){
-                $nouvelleQtt = $qtt++;
-                $nouvelleQtt =+ array_replace($product['qtt']);
-            };
-
+            //Envoie un message lorsque l'utilisateur a ajouté un produit  
             $_SESSION['alert'] = "<p class='alert alert-success w-25' role='alert'>Le produit ".$product['name']." a bien été ajouté !</p>";
-
+            
+            // On enregistre le produit nouvellement créer dans la superglobale SESSION a qui on fournit une clef products, $_SESSION nous renverra donc un tableau associatif products => product
             $_SESSION['products'][] = $product;
 
-        } else {
+            // fonction qui trie le tableau par ordre alphabétique
+            sort($_SESSION['products']);
 
+        } else { //Les filtres ont renvoyé false, null ou 0
+
+            // envoie un message 
             $_SESSION['alert'] = "<p class='alert alert-warning w-25 ' role='alert'>Votre produit n'a pas été ajouté, il est incorrect ! </p>";
         }
     }
 
-    header("Location:index.php");
+    header("Location:index.php"); //Redirection vers index.php pour que l'utilisateur ne puisse pas atteindre la page traitement.php
