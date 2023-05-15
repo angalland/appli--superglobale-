ada0cf45747840c4cbe02c354d887089193d60b2
angalland <angalland@laposte.net>
2023-05-15 09:16:00 +0200

    ajout de commentaire

--- supprimer.php
+++ supprimer.php
@@ -1,14 +1,14 @@
 <?php
 
-session_start();
+session_start(); // fait appelle a la session
 
-if (isset($_GET['supprimer'])){
-    $indexClef = $_GET['index'];
-    unset($_SESSION['products'][$indexClef]);
+if (isset($_GET['supprimer'])){ // si $_GET['supprimer'] est déclarer = si on appuie sur le bouton supprimer alors
+    $indexClef = $_GET['index']; // crée une variable qui prend la valeur de l'index
+    unset($_SESSION['products'][$indexClef]); // supprime du tableau $_SESSION les éléments ayant l'index pris audessus
 }
 
-if (isset($_GET['supprimerTableau'])){
-    unset($_SESSION['products']);
+if (isset($_GET['supprimerTableau'])){ // Si $_GE['supprimerTableau'] est déclarer = si on appuie sur le bouton supprimer tableau alors
+    unset($_SESSION['products']); // supprime $_SESSION['products']
 }
 
-header('Location:recap.php');
+header('Location:recap.php'); // renvoie a la page recap.php cette page est inaccessible pour l'utilisateur
