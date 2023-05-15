f46cde3aaafd56612945c56db798b2b74e1d4224
angalland <angalland@laposte.net>
2023-05-15 15:56:34 +0200

    ajout methode files dans indexTuto

--- importerImage/indexTuto.php
+++ importerImage/indexTuto.php
@@ -1,12 +1,35 @@
 <?php
 
+    var_dump($_FILES)."</br>";
+
     if (isset($_FILES["file"])){
         $tmpName = $_FILES["file"]['tmp_name'];
         $name = $_FILES["file"]["name"];
         $type = $_FILES["file"]["type"];
         $error = $_FILES["file"]["error"];
         $size = $_FILES["file"]["size"];
-    
+
+        //PhotoAnthony1.jpg = $name
+        //['PhotoAnthony1', 'jpg']
+        $tabExtension = explode('.', $name);
+        // var_dump($tabExtension)."</br>";
+        $extension = strtolower(end($tabExtension));
+        // var_dump($extension);
+        $tailleMax = 3000000;
+
+        //Tableau des extensions qu'on autorise 
+        $extensionAutorisees = ['jpg', 'jpeg', 'gif', 'png'];
+        
+        if(in_array($extension, $extensionAutorisees) && $size <= $tailleMax && $error == 0 ){
+            $uniqueName = uniqid('', true);
+            $fileName = $uniqueName.'.'.$extension;
+            move_uploaded_file($tmpName, './upload/'.$fileName);
+        } else {
+            echo "mauvaise extension ou taille trop importante ou erreure";
+        }
+
+
+        
     }
 ?>
 
